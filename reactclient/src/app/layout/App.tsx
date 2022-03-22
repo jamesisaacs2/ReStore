d9150eb43f746c14e940e81530453b8f0f3ec156
James Isaacs <jkifao@gmail.com>
2022-03-22 09:02:05 -0700

    added storeContext hook and provider for global app access to basket

--- reactclient/src/app/layout/App.tsx
+++ reactclient/src/app/layout/App.tsx
@@ -30,25 +30,23 @@ function App() {
 	}
 
 	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<ToastContainer position="bottom-right" hideProgressBar />
-				<CssBaseline />
-				<Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-				<Container>
-					<Switch>
-						<Route exact path="/" component={HomePage} />
-						<Route exact path="/catalog" component={Catalog} />
-						<Route path="/catalog/:id" component={ProductDetails} />
-						<Route path="/about" component={AboutPage} />
-						<Route path="/contact" component={ContactPage} />
-						<Route path="/server-error" component={ServerError} />
-						<Route path="/basket" component={BasketPage} />
-						<Route component={NotFound} />
-					</Switch>
-				</Container>
-			</ThemeProvider>
-		</>
+		<ThemeProvider theme={theme}>
+			<ToastContainer position="bottom-right" hideProgressBar />
+			<CssBaseline />
+			<Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+			<Container>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/catalog" component={Catalog} />
+					<Route path="/catalog/:id" component={ProductDetails} />
+					<Route path="/about" component={AboutPage} />
+					<Route path="/contact" component={ContactPage} />
+					<Route path="/server-error" component={ServerError} />
+					<Route path="/basket" component={BasketPage} />
+					<Route component={NotFound} />
+				</Switch>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
