5d5850685c873ba0496fb5089e417d0bacd8d785
James Isaacs <jkifao@gmail.com>
2022-03-28 23:14:40 -0700

    added search and filters (brand, type) elements to catalog page, and updated redux logic flow for query components

--- reactclient/src/app/api/agent.ts
+++ reactclient/src/app/api/agent.ts
@@ -9,13 +9,6 @@ axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const requests = {
-	get: (url: string) => axios.get(url).then(responseBody),
-	post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-	put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-	delete: (url: string) => axios.delete(url).then(responseBody),
-};
-
 axios.interceptors.response.use(
 	async (response) => {
 		await delay();
@@ -56,8 +49,15 @@ axios.interceptors.response.use(
 	}
 );
 
+const requests = {
+	get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
+	post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+	put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+	delete: (url: string) => axios.delete(url).then(responseBody),
+};
+
 const Catalog = {
-	list: () => requests.get("products"),
+	list: (params: URLSearchParams) => requests.get("products", params),
 	details: (id: number) => requests.get(`products/${id}`),
 	fetchFilters: () => requests.get("products/filters"),
 };
--- reactclient/src/features/catalog/catalogSlice.ts
+++ reactclient/src/features/catalog/catalogSlice.ts
@@ -1,15 +1,36 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
-import { Product } from "../../app/models/product";
+import { Product, ProductParams } from "../../app/models/product";
 import { RootState } from "../../app/store/configureStore";
+
+interface CatalogState {
+	productsLoaded: boolean;
+	filtersLoaded: boolean;
+	status: string;
+	brands: string[];
+	types: string[];
+	productParams: ProductParams;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
-export const fetchProductsAsync = createAsyncThunk<Product[]>(
+function getAxiosParams(productParams: ProductParams) {
+	const params = new URLSearchParams();
+	params.append("pageNumber", productParams.pageNumber.toString());
+	params.append("pageSize", productParams.pageSize.toString());
+	params.append("orderBy", productParams.orderBy);
+	if (productParams.searchTerm) params.append("searchTerm", productParams.searchTerm);
+	if (productParams.brands) params.append("brands", productParams.brands.toString());
+	if (productParams.types) params.append("types", productParams.types.toString());
+	return params;
+}
+
+export const fetchProductsAsync = createAsyncThunk<Product[], void, { state: RootState }>(
 	"catalog/fetchProductsAsync",
 	async (_, thunkAPI) => {
+		const params = getAxiosParams(thunkAPI.getState().catalog.productParams);
 		try {
-			return await agent.Catalog.list();
+			return await agent.Catalog.list(params);
 		} catch (error: any) {
 			console.log("fetchProduct-s-Async", error);
 			return thunkAPI.rejectWithValue({ error: error.data });
@@ -38,16 +59,33 @@ export const fetchFilters = createAsyncThunk("catalog/fetchFilters", async (_, t
 	}
 });
 
+function initParams() {
+	return {
+		pageNumber: 1,
+		pageSize: 6,
+		orderBy: "name",
+	};
+}
+
 export const catalogSlice = createSlice({
 	name: "catalog",
-	initialState: productsAdapter.getInitialState({
+	initialState: productsAdapter.getInitialState<CatalogState>({
 		productsLoaded: false,
 		filtersLoaded: false,
 		status: "idle",
 		brands: [],
 		types: [],
+		productParams: initParams(),
 	}),
-	reducers: {},
+	reducers: {
+		setProductParams: (state, action) => {
+			state.productsLoaded = false;
+			state.productParams = { ...state.productParams, ...action.payload };
+		},
+		resetProductParams: (state) => {
+			state.productParams = initParams();
+		},
+	},
 	extraReducers: (builder) => {
 		builder.addCase(fetchProductsAsync.pending, (state) => {
 			state.status = "pendingFetchProductsAsync";
@@ -91,3 +129,5 @@ export const catalogSlice = createSlice({
 export const productSelectors = productsAdapter.getSelectors(
 	(state: RootState) => state.catalog
 );
+
+export const { setProductParams, resetProductParams } = catalogSlice.actions;
