56cea0345ca80788d9514891e8cd3e6dbc149ec6
James Isaacs <jkifao@gmail.com>
2022-04-02 13:16:47 -0700

    added orders framework, shipping address, etc - premigration

---
+++ API/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities.OrderAggregate
+{
+   [Owned]
+   public class Order
+   {
+      public int Id { get; set; }
+      public string BuyerId { get; set; }
+      public ShippingAddress ShippingAddress { get; set; }
+      public DateTime OrderDate { get; set; } = DateTime.Now;
+      public List<OrderItem> OrderItems { get; set; }
+      public long Subtotal { get; set; }
+      public long DeliveryFee { get; set; }
+      public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+      public long GetTotal()
+      {
+         return Subtotal + DeliveryFee;
+      }
+
+
+   }
+}
