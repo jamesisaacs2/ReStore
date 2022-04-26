1e9bbcbdf206c09154b976aa29a4c3789f83321d
James Isaacs <jkifao@gmail.com>
2022-04-25 23:01:37 -0700

    added inventory view page and protected route

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -71,7 +71,8 @@ namespace API.Controllers
          {
             var imageResult = await _imageService.AddImageAsync(productDto.File);
 
-            if (imageResult.Error != null) return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+            if (imageResult.Error != null)
+               return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
 
             product.PictureUrl = imageResult.SecureUrl.ToString();
             product.PublicId = imageResult.PublicId;
@@ -88,7 +89,7 @@ namespace API.Controllers
 
       [Authorize(Roles = "Admin")]
       [HttpPut]
-      public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+      public async Task<ActionResult<Product>> UpdateProduct([FromForm] UpdateProductDto productDto)
       {
          var product = await _context.Products.FindAsync(productDto.Id);
 
@@ -96,9 +97,23 @@ namespace API.Controllers
 
          _mapper.Map(productDto, product);
 
+         if (productDto.File != null)
+         {
+            var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+            if (imageResult.Error != null)
+               return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+            if (!string.IsNullOrEmpty(product.PublicId))
+               await _imageService.DeleteImageAsync(product.PublicId);
+
+            product.PictureUrl = imageResult.SecureUrl.ToString();
+            product.PublicId = imageResult.PublicId;
+         }
+
          var result = await _context.SaveChangesAsync() > 0;
 
-         if (result) return NoContent();
+         if (result) return Ok(product);
 
          return BadRequest(new ProblemDetails { Title = "Problem updating product" });
       }
@@ -110,6 +125,9 @@ namespace API.Controllers
          var product = await _context.Products.FindAsync(id);
 
          if (product == null) return NotFound();
+
+         if (!string.IsNullOrEmpty(product.PublicId))
+            await _imageService.DeleteImageAsync(product.PublicId);
 
          _context.Products.Remove(product);
 
---
+++ reactclient/src/app/components/AppSelectList.tsx
@@ -0,0 +1,34 @@
+import {
+	Box,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	FormHelperText,
+} from "@mui/material";
+import { useController, UseControllerProps } from "react-hook-form";
+
+interface Props extends UseControllerProps {
+	label: string;
+	items: string[];
+}
+
+export default function AppSelectList(props: Props) {
+	const { fieldState, field } = useController({ ...props, defaultValue: "" });
+
+	return (
+		<Box sx={{ minWidth: 120 }}>
+			<FormControl fullWidth error={!!fieldState.error}>
+				<InputLabel>{props.label}</InputLabel>
+				<Select value={field.value} label={props.label} onChange={field.onChange}>
+					{props.items.map((item, index) => (
+						<MenuItem key={index} value={item}>
+							{item}
+						</MenuItem>
+					))}
+				</Select>
+				<FormHelperText>{fieldState.error?.message}</FormHelperText>
+			</FormControl>
+		</Box>
+	);
+}
