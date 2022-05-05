d445b2f5eee4593f9a6a71d71633fdbca31d3607
James Isaacs <jkifao@gmail.com>
2022-05-04 23:53:47 -0700

    converting Program to .NET 6, started middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,50 +1,163 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Entities.OrderAggregate;
+using API.RequestHelpers;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+
+builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+builder.Services.AddSwaggerGen(config =>
 {
-   public class Program
+   var c = config;
+
+   c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+   c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
    {
-      public static async Task Main(string[] args)
-      {
-         var host = CreateHostBuilder(args).Build();
-         using var scope = host.Services.CreateScope();
-         var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
-         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+      Description = "Jwt auth header",
+      Name = "Authorization",
+      In = ParameterLocation.Header,
+      Type = SecuritySchemeType.ApiKey,
+      Scheme = "Bearer"
+   });
 
-         try
-         {
-            await context.Database.MigrateAsync();
-            await DbInitializer.Initialize(context, userManager);
-         }
-         catch (Exception ex)
-         {
-            logger.LogError(ex, "Problem migrating data");
-         }
+   c.AddSecurityRequirement(new OpenApiSecurityRequirement
+   {
+               {
+                  new OpenApiSecurityScheme
+                  {
+                     Reference = new OpenApiReference
+                     {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                     },
+                     Scheme = "oauth2",
+                     Name = "Bearer",
+                     In = ParameterLocation.Header
+                  },
+                  new List<string>()
+               }
+   });
+});
 
-         await host.RunAsync();
-      }
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+   var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-      public static IHostBuilder CreateHostBuilder(string[] args) =>
-          Host.CreateDefaultBuilder(args)
-              .ConfigureWebHostDefaults(webBuilder =>
-              {
-                 webBuilder.UseStartup<Startup>();
-              });
+   string connStr;
+
+   if (env == "Development")
+   {
+      // Use connection string from file.
+      connStr = builder.Configuration.GetConnectionString("DefaultConnection");
    }
-}
+   else
+   {
+      // Use connection string provided at runtime by Heroku.
+      var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-// In Main: old way (now uses "using" above):
-// finally
+      // Parse connection URL to connection string for Npgsql
+      connUrl = connUrl.Replace("postgres://", string.Empty);
+      var pgUserPass = connUrl.Split("@")[0];
+      var pgHostPortDb = connUrl.Split("@")[1];
+      var pgHostPort = pgHostPortDb.Split("/")[0];
+      var pgDb = pgHostPortDb.Split("/")[1];
+      var pgUser = pgUserPass.Split(":")[0];
+      var pgPass = pgUserPass.Split(":")[1];
+      var pgHost = pgHostPort.Split(":")[0];
+      var pgPort = pgHostPort.Split(":")[1];
+
+      connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
+   }
+
+   // Whether the connection string came from the local development configuration file
+   // or from the environment variable from Heroku, use it to set up your DbContext.
+   options.UseNpgsql(connStr);
+});
+
+builder.Services.AddCors();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+   options.User.RequireUniqueEmail = true;
+   options.Password.RequiredLength = 13;
+})
+   .AddRoles<Role>()
+   .AddEntityFrameworkStores<StoreContext>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(options =>
+   {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+         ValidateIssuer = false,
+         ValidateAudience = false,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+      };
+   });
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<ImageService>();
+
+// Add middleware
+var app = builder.Build();
+
+// namespace API
 // {
-//    scope.Dispose();
+//    public class Program
+//    {
+//       public static async Task Main(string[] args)
+//       {
+//          var host = CreateHostBuilder(args).Build();
+//          using var scope = host.Services.CreateScope();
+//          var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+//          var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+//          var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+//          try
+//          {
+//             await context.Database.MigrateAsync();
+//             await DbInitializer.Initialize(context, userManager);
+//          }
+//          catch (Exception ex)
+//          {
+//             logger.LogError(ex, "Problem migrating data");
+//          }
+
+//          await host.RunAsync();
+//       }
+
+//       public static IHostBuilder CreateHostBuilder(string[] args) =>
+//           Host.CreateDefaultBuilder(args)
+//               .ConfigureWebHostDefaults(webBuilder =>
+//               {
+//                  webBuilder.UseStartup<Startup>();
+//               });
+//    }
 // }
+
